e2429fba0f7f9977fad9097e2bc42403e78b62ef
sebastian_cardona3 <sebastian.co.dev@gmail.com>
2021-03-01 16:09:56 -0500

    Add enpoint for find a client. Closes #6

--- apps/debts/src/main/java/com/dizquestudios/evertectest/apps/debts/api/ClientController.java
+++ apps/debts/src/main/java/com/dizquestudios/evertectest/apps/debts/api/ClientController.java
@@ -1,18 +1,21 @@
 package com.dizquestudios.evertectest.apps.debts.api;
 
-import com.dizquestudios.evertectest.core.debts.api.ClientAPI;
 import java.util.List;
 
+import org.springframework.http.MediaType;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.http.HttpStatus;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dizquestudios.evertectest.core.debts.api.ClientAPI;
 import com.dizquestudios.evertectest.core.debts.domain.Client;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import org.springframework.http.MediaType;
 
 /**
  *
@@ -21,6 +24,11 @@ import org.springframework.http.MediaType;
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+            public record ClientRequest(@JsonProperty("id") String id) {
+
+    }
 
     private final ClientAPI api;
 
@@ -34,4 +42,15 @@ public class ClientController {
     public ResponseEntity<List<Client>> getAll() {
         return new ResponseEntity<>(api.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/client", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("Find client information.")
+    @ApiResponse(code = 200, message = "OK")
+    public ResponseEntity<Client> findClientById(@RequestBody ClientRequest request) {
+
+        return api.findClient(request.id())
+                .map(client -> new ResponseEntity<>(client, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 }
--- core/Core-Debts/src/main/java/com/dizquestudios/evertectest/core/debts/api/ClientAPI.java
+++ core/Core-Debts/src/main/java/com/dizquestudios/evertectest/core/debts/api/ClientAPI.java
@@ -1,28 +1,35 @@
 package com.dizquestudios.evertectest.core.debts.api;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.dizquestudios.evertectest.core.debts.domain.Client;
 import com.dizquestudios.evertectest.core.debts.domain.ClientRepository;
 import com.dizquestudios.evertectest.core.debts.shared.API;
 
 /**
  * Public API for Client.
+ *
  * @author Sebastian
  */
 @API
 public class ClientAPI {
-    
+
     private final ClientRepository repository;
 
     public ClientAPI(ClientRepository repository) {
         this.repository = repository;
     }
 
+    public Optional<Client> findClient(String id) {
+        return this.repository.findClient(id);
+    }
+
     public List<Client> findAll() {
         return repository.findAll();
     }
-    
-    public void saveList(List<Client> clients){
+
+    public void saveList(List<Client> clients) {
         repository.saveList(clients);
     }
 
